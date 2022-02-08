62cc7bbfb6badf2b31c20ab2b28e3f8d183762b3
under-rise <suzuki@under-rise.com>
2022-02-08 12:05:20 +0900

    ファイルphp変更

---
+++ tpl/header_serach.php
@@ -0,0 +1,15 @@
+<header>
+		<h1>日本最大級のBBQプラットフォーム Bavi</h1>
+		
+		<a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
+		
+		<div>
+			<p>日本全国のBBQ場を検索！</p>
+			
+			<form>
+				<input type="text" placeholder="フリーワード">
+				<input type="submit" value="検 索">
+			</form>
+		</div>
+	</header>
+	
