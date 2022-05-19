03de9c95407ba9e94da988fa6b5fc0ed7e8e65f4
under-rise <suzuki@under-rise.com>
2022-05-19 13:49:44 +0900

    コンシェルジュページのステップの簡略化

--- resources/js/concierge.js
+++ resources/js/concierge.js
@@ -1,3 +1,4 @@
+
 /* datepicker入力欄をfocus時にreadonlyにする */
 $(function(){
 	$('.datepicker').click(function(){
@@ -5,61 +6,59 @@ $(function(){
 	});
 });
 
-
 /* コンシェルジュページのステップフォーム */
 $(function() {
 	var num=0;     //現在のステップ数を格納する変数
 	var edit_num;  //修正するステップ数を格納する変数
-	
-	/* 現在のステップ数を取得 */
-	$('#step form ol > li').on('click', function(){
-		num = $('#step form ol > li').index(this);
-	});
-	
+	let stepLength = 0; //STEPの数
+
+	// test
+	init();
+
 	/* 「次へ」をクリックした時の処理 */
 	$(document).on('click', 'button.next.active', function(){
 		$(window).scrollTop($('#step').offset().top);
 		slideLeft();
-		if(edit_num<14){
-			$('#step_indicater img').hide();
-		}else if(num<15){
-			$('#step_indicater img').attr('src','resources/images/concierge/step' + (num+1) + '.svg');
-		}else{
-			$('#step_indicater img').hide();
-		}
 	});
 	
 	/* 「戻る」をクリックした時の処理 */
 	$(document).on('click', 'button.prev', function(){
 		$(window).scrollTop($('#step').offset().top);
 		slideRight();
-		$('#step_indicater img').attr('src','resources/images/concierge/step'+ (num+1) +'.svg');
 	});
+
 	
 	/* 確認画面の「編集」をクリックした時の処理 */
 	$(document).on('click', '#confirm button.edit', function(){
 		edit_num = $('button.edit').index(this);
-		$('#step form ol > li').eq(15).addClass('right')
-		$('#step form ol > li').eq(edit_num).removeClass('left')
-		$('#step form ol > li').css('height','auto');
-		$('#step form ol > li').eq(edit_num).find('.prev').hide();
-		$('#step_indicater img').show();
-		$('#step_indicater img').attr('src','resources/images/concierge/step' + (edit_num+1) + '.svg');
+		num = $('button.edit').index(this);
+		$('#step form ol > li').removeClass('right left');
+		$('#step form ol > li:nth-child(-n + '+(num)+')').addClass('left');
+		$('#step form ol > li:nth-child(n + '+(num+2)+')').addClass('right');
+
+		$('#step_indicater').show();
+		changeStep();
 		$(window).scrollTop($('#step').offset().top);
 		$('button.next.active').html('内容を確認する');
+		$('button.next.active').addClass('yellow');
+		stepHeightAjust();
 	});
 	
 	/* テキスト入力でキーが押されたときの処理 */
 	$("input[type='text']").keyup(function(){
 		if($(this).val()!=''){
-			if(num==4 || edit_num==4){
+			if(num==2 || edit_num==2){
 				var er=0;
-				$('#step form ol > li').eq(4).find('input[type="text"]').each(function(){
+				$('#step form ol > li').eq(2).find('input[type="text"]').each(function(){
 					var value = $(this).val();
-					if (value == "" || !value.match(/[^\s\t]/))er++;
+					if (value == "" || !value.match(/[^\s\t]/)){
+						er++;
+					}else{
+					
+					}
 				});
 				if(er==0){
-					$('#step form ol > li').eq(4).find('dd').addClass('active');
+					$('#step form ol > li').eq(2).find('dd').addClass('active');
 				}
 			}else{
 				$(this).parent().addClass('active');
@@ -71,9 +70,10 @@ $(function() {
 				$(this).parent().removeClass('active');
 			}
 		}
-		if(validate() && num!=9){
+		// 予算
+		if(validate() && num!=4){
 			$('#step form ol > li').eq(num).find('.next').addClass('active')
-		}else if(num==9){
+		}else if(num==4){
 			var error_nine = 0;
 			$('#step form ol > li').eq(num).find('input[type="text"]').each(function(){
 				if ($(this).val() == "" || !$(this).val().match(/[^\s\t]/))error_nine++;
@@ -164,6 +164,14 @@ $(function() {
 		var attr_name = $(this).attr("name");
 		$('#confirm span[data=\"' + attr_name + '\"]').html($('input:radio[name=' + attr_name + ']:checked').val());
 	});
+
+	/* 詳細ボタン */
+	$('.toggle_btn').on('click',function(){
+		$(this).hide();
+		$(this).next().slideToggle('first',function(){
+			stepHeightAjust();
+		});
+	});
 	
 	/* チェックボックスが押された時の処理 */
 	$("input:checkbox").change(function() {
@@ -213,39 +221,118 @@ $(function() {
 		var attr_name = $(this).attr("name");
 		$('#confirm span[data=' + attr_name + ']').html($(this).val());
 	});
+
+	/* プラン選択の処理 */
+	$('#plan_checkbox input').change(function(){
+
+		const obj = $(this).closest('#plan_checkbox').parent();
+		$(obj).find('.plan').hide();
+		
+		let plan_array = [];
+		$('#plan_checkbox :checkbox:checked').each(function() {
+			var plan = $(this).data('plan');
+			plan_array.push(plan);
+			// $(obj).find('.'+plan).show();
+		});
+
+		// 道具のみ
+		if(plan_array.includes('plan01')){
+			$(obj).find('.plan_item').show();
+		}
+		// 場所＋食事
+		if(plan_array.includes('plan02')){
+			$(obj).find('.plan_item,.plan_food').show();
+		}
+		// 場所＋食事+ドリンク
+		if(plan_array.includes('plan03')){
+			$(obj).find('.plan_item,.plan_food,.plan_drink').show();
+		}
+		// オリジナルプラン
+		if(plan_array.includes('plan04')){
+			$(obj).find('.plan_original').show();
+		}
 	
+		stepHeightAjust();
+		
+	});
+
 	/* 次のステップへ進む */
 	function slideLeft(){
-		if(num<15){
+		if(num<stepLength){
 			$('#step form ol > li').eq(num).addClass('left');
 			$('#step form ol > li').eq(num+1).removeClass('right');
 			num++;
+			stepHeightAjust();
+			changeStep();
+			
 		}
-		
-		if(num==15){
-			$('#step form ol > li').css('height',$('#step form ol > li:last-child #confirm').height() + 140 +'px');
-		}
-		if(edit_num<15){
-			$('#step form ol > li').eq(edit_num).addClass('left');
-			$('#step form ol > li').eq(15).removeClass('right');
-			$('#step form ol > li').css('height',$('#step form ol > li:last-child #confirm').height() + 140 +'px');
-
-		}
-	}
 	
+		//確認ページ
+		if(num==stepLength){
+			// $('#step form ol > li').css('height',$('#step form ol > li:last-child #confirm').height() + 140 +'px');
+			stepHeightAjust
+			$('#step_indicater').hide()
+		}
+
+		//確認・修正後
+		if(edit_num != undefined){
+			num = stepLength;
+			changeStep();
+			$('#step_indicater').hide()
+			$('#step form ol > li').addClass('left');
+			$('#step form ol > li').eq(stepLength).removeClass('left right');
+			stepHeightAjust();
+			// $('#step form ol > li').css('height',$('#step form ol > li:last-child #confirm').height() + 140 +'px');
+		}
+
+	
+	}
+
 	/* 前のステップへ戻る */
 	function slideRight(){
 		$('#step form ol > li').eq(num).addClass('right');
 		$('#step form ol > li').eq(num-1).removeClass('left');
 		num--;
+		stepHeightAjust();
+		changeStep();
 	}
 	
 	/* 確認画面高さ制御 */
 	$(window).resize(function(){
-		if(num==15 && !$('#step form ol > li').eq(15).hasClass('right')){
+		
+		if(num==stepLength && !$('#step form ol > li').eq(stepLength).hasClass('right')){
 			$('#step form ol > li').css('height',$('#step form ol > li:last-child #confirm').height() + 140 +'px');
 		}
 	});
+
+
+	/* スタート関数 */
+	function init(){
+		stepHeightAjust();
+		stepLength = $('#step form ol > li').length -1;
+		changeStep();
+		$('#step form ol > li').removeClass('right left');
+		$('#step form ol > li:nth-child(-n + '+(num)+')').addClass('left');
+		$('#step form ol > li:nth-child(n + '+(num+2)+')').addClass('right');
+	}
+
+	/* 高さ調整*/
+	function stepHeightAjust(){
+		const padd = 50;
+		let h = $('#step form ol > li').eq(num).innerHeight();
+		$('#step form ol').css({'height':h+padd+"px"});
+	}
+
+	/* STEP処理 */
+	function changeStep(){
+		$('#step_indicater div span').text(num+1);
+		let w = $('#step_indicater div').innerWidth() / 2;
+		// let pos = num / stepLength*100;
+		let pos = (num+1) == stepLength ? `calc(100% * 0.9)` : num / stepLength*100 + '%';
+		// $('#step_indicater div').css({'left':`calc(${pos}% + ${w}px)`});
+		$('#step_indicater div').css({'left': pos});
+		$('span.step_num').text(num+1+'/'+stepLength);
+	}
 	
 	/* バリデーション */
 	function validate(){
@@ -288,5 +375,9 @@ $(function() {
 		}
 		return true;
 	}
+
+	
+
+
 });
 
