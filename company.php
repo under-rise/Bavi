173c8f60c94541db4d71a091dd93094fa7460604
under-rise <suzuki@under-rise.com>
2023-04-10 13:37:18 +0900

    会社概要ページ修正

--- company.php
+++ company.php
@@ -1,181 +1,234 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="format-detection" content="telephone=no">
-	<meta name="description" content="">
-	<title></title>
-	<link rel="stylesheet" href="resources/css/sanitize.css">
-	<link rel="stylesheet" href="resources/css/base.css">
-	<link rel="stylesheet" href="resources/css/base_sp.css">
-	<link rel="stylesheet" href="resources/css/company.css">
-	<link rel="stylesheet" href="resources/css/company_sp.css">
-	<script type="text/javascript" src="resources/js/jquery-1.11.2.min.js"></script>
-	<script type="text/javascript" src="resources/js/config.js"></script>
-	<script type="text/javascript" src="resources/js/smoothScroll.js"></script>
-	<!--[if lt IE 9]>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="">
+    <title></title>
+    <link rel="stylesheet" href="resources/css/sanitize.css">
+    <link rel="stylesheet" href="resources/css/base.css">
+    <link rel="stylesheet" href="resources/css/base_sp.css">
+    <link rel="stylesheet" href="resources/css/company.css">
+    <link rel="stylesheet" href="resources/css/company_sp.css">
+    <script type="text/javascript" src="resources/js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="resources/js/config.js"></script>
+    <script type="text/javascript" src="resources/js/smoothScroll.js"></script>
+    <!--[if lt IE 9]>
 	<script type="text/javascript" src="resources/js/html5shiv.js"></script>
 	<![endif]-->
-	
+
 </head>
+
 <body>
-<div id="loading"></div>
-<div id="wrapper">
-	<header class="no_fixed">
-		<h1>日本最大級のBBQプラットフォーム Bavi</h1>
-		
-		<a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
-	</header>
-	
-	
-	<article class="no_fixed cont">
-		<h2>会社概要</h2>
-		
-		<section class="cont">
-			<h3>会社概要</h3>
-			
-			<dl>
-				<dt>会社名</dt>
-				<dd>ゼネラルパーソンカンパニー株式会社</dd>
-				
-				<dt>本社住所</dt>
-				<dd>〒120-0001 <br class="only_smart" />東京都足立区大谷田3-21-6</dd>
-				
-				<dt>電話番号</dt>
-				<dd>TEL. 03-5856-2030</dd>
-				
-				<dt>FAX番号</dt>
-				<dd>FAX. 03-5856-1780</dd>
-				
-				<dt>事業内容</dt>
-				<dd>
-					<ul>
-						<li>「手軽にＢＢＱ.com」団体向け出張ＢＢＱ</li>
-						<li>「江戸餅つき屋」餅つきのトータルサービス</li>
-						<li>「楽々荷造りサービス」引越時の負担軽減サービス</li>
-						<li>「運送事業」一般貨物運送事業全般</li>
-					</ul>
-				</dd>
-				
-				<dt>事業登録</dt>
-				<dd>
-					<ul>
-						<li>一般貨物自動車運送事業 関自貨第815号</li>
-						<li>飲食店営業許可 29足足保生食第00560号</li>
-					</ul>
-				</dd>
-				
-				<dt>法人設立年月日</dt>
-				<dd>平成27年5月19日</dd>
-				
-				<dt>従業員数</dt>
-				<dd>50人(PA含む)</dd>
-			</dl>
-		</section>
-		
-		
-		<section class="cont">
-			<h3>アクセス</h3>
-			
-			<div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25894.02212617083!2d139.82338557239575!3d35.78144903197983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188fe5be682f73%3A0x972b9c86571fcb8b!2z44CSMTIwLTAwMDEg5p2x5Lqs6YO96Laz56uL5Yy65aSn6LC355Sw77yT5LiB55uu77yS77yR4oiS77yW!5e0!3m2!1sja!2sjp!4v1615721737822!5m2!1sja!2sjp" width="640" height="380" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
-			
-			<p>〒120-0001 東京都足立区大谷田3-21-6</p>
-		</section>
-	</article>
-	
-	
-	<footer>
-		<a href="#wrapper" id="topbtn"><img src="resources/images/to_top.png" alt="ページTOPへ" /></a>
-		
-		<figure>
-			<img src="resources/images/foot_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" />
-		</figure>
-		
-		<ul>
-			<li><a href=""><img src="resources/images/sns1.png" alt="Twitter" /></a></li>
-			<li><a href=""><img src="resources/images/sns2.png" alt="You tube" /></a></li>
-			<li><a href=""><img src="resources/images/sns3.png" alt="facebook" /></a></li>
-			<li><a href=""><img src="resources/images/sns4.png" alt="LINE" /></a></li>
-		</ul>
-		
-		<nav>
-			<ul>
-				<li><a href="/about.html">Baviについて</a></li>
-				<li><a href="/contact.html">お問い合わせ</a></li>
-				<li><a href="/company.html">運営会社</a></li>
-			</ul>
-		</nav>
-		
-		<p>&copy; Copyright Bavi all right reserved.</p>
-	</footer>
-	
-	
-	<nav id="bottom_nav" >
-		<ul>
-			<li>
-				<a href="/">
-					<img src="resources/images/bottom_nav1.png" alt="BBQ場" />
-					<span>BBQ場</span>
-				</a>
-			</li>
-			<li>
-				<a href="/spot.html">
-					<img src="resources/images/bottom_nav2.png" alt="食材SPOT" />
-					<span>食材SPOT</span>
-				</a>
-			</li>
-			<li>
-				<a href="/column.html">
-					<img src="resources/images/bottom_nav3.png" alt="COLUMN" />
-					<span>COLUMN</span>
-				</a>
-			</li>
-			<li>
-				<a href="/like.html">
-					<img src="resources/images/bottom_nav4.png" alt="LIKE" />
-					<span>LIKE</span>
-				</a>
-			</li>
-			<li>
-				<a id="menu_open">
-					<img src="resources/images/bottom_nav5.png" alt="MENU" />
-					<span>MENU</span>
-				</a>
-			</li>
-		</ul>
-	</nav>
-	
-	
-	<nav id="overlay_nav" >
-		<span id="close">+</span>
-		
-		<div>
-			<a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
-			
-			<dl>
-				<dt>探す</dt>
-				<dd><a href="/">BBQ場</a></dd>
-				<dd><a href="/spot.html">周辺店舗</a></dd>
-				<dd><a href="/column.html">コラム</a></dd>
-				
-				<dt>MyPage</dt>
-				<dd><a href="">お気に入りページ</a></dd>
-				
-				<dt>新着情報</dt>
-				<dd><a href="">BBQ場新着</a></dd>
-				<dd><a href="">周辺店舗新着</a></dd>
-				
-				<dt>その他</dt>
-				<dd><a href="/about.html">Baviについて</a></dd>
-				<dd><a href="/company.html">運営会社</a></dd>
-				<dd><a href="/contact.html">お問い合わせ</a></dd>
-				<dd><a href="/company.html">運営会社</a></dd>
-				<dd><a href="/contact.html">お問い合わせ</a></dd>
-			</dl>
-		</div>
-	</nav>
-</div>
+    <div id="loading"></div>
+    <div id="wrapper">
+        <header class="no_fixed">
+            <h1>日本最大級のBBQプラットフォーム Bavi</h1>
+
+            <a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
+        </header>
+
+
+        <article class="no_fixed cont">
+            <h2>会社概要</h2>
+
+            <section class="cont">
+                <h3>会社概要</h3>
+
+                <dl>
+                    <dt>会社名</dt>
+                    <dd>ゼネラルパーソンカンパニー株式会社</dd>
+
+                    <dt>本社住所</dt>
+                    <dd>〒120-0001 <br class="only_smart" />東京都足立区大谷田3-21-6</dd>
+
+                    <dt>電話番号</dt>
+                    <dd>TEL. 03-5856-2030</dd>
+
+                    <dt>FAX番号</dt>
+                    <dd>FAX. 03-5856-1780</dd>
+
+                    <dt>事業内容</dt>
+                    <dd>
+                        <ul>
+                            <li>「手軽にＢＢＱ.com」団体向け出張ＢＢＱ</li>
+                            <li>「江戸餅つき屋」餅つきのトータルサービス</li>
+                            <li>「楽々荷造りサービス」引越時の負担軽減サービス</li>
+                            <li>「運送事業」一般貨物運送事業全般</li>
+                        </ul>
+                    </dd>
+
+                    <dt>事業登録</dt>
+                    <dd>
+                        <ul>
+                            <li>一般貨物自動車運送事業 関自貨第815号</li>
+                            <li>飲食店営業許可 29足足保生食第00560号</li>
+                        </ul>
+                    </dd>
+
+                    <dt>法人設立年月日</dt>
+                    <dd>平成27年5月19日</dd>
+
+                    <dt>従業員数</dt>
+                    <dd>50人(PA含む)</dd>
+                </dl>
+            </section>
+
+            <h3>アクセス</h3>
+
+            <div><iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25894.02212617083!2d139.82338557239575!3d35.78144903197983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188fe5be682f73%3A0x972b9c86571fcb8b!2z44CSMTIwLTAwMDEg5p2x5Lqs6YO96Laz56uL5Yy65aSn6LC355Sw77yT5LiB55uu77yS77yR4oiS77yW!5e0!3m2!1sja!2sjp!4v1615721737822!5m2!1sja!2sjp"
+                    width="640" height="380" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+
+            <p>〒120-0001 東京都足立区大谷田3-21-6</p>
+
+
+
+            <section class="cont">
+                <h3>会社概要</h3>
+
+                <dl>
+                    <dt>会社名</dt>
+                    <dd>Bavi Japan 株式会社</dd>
+
+                    <dt>本社住所</dt>
+                    <dd>〒889-4155 宮崎県えびの市大字亀沢280-3</dd>
+
+                    <dt>電話番号</dt>
+                    <dd>TEL. 03-5856-2030</dd>
+
+                    <dt>FAX番号</dt>
+                    <dd>FAX. 03-5856-1780</dd>
+
+                    <dt>事業内容</dt>
+                    <dd>
+                        <ul>
+                            <li><strong>日本最大級BBQプラットフォームBaviの運営</strong></li>
+                            <li>BBQ施設、キャンプ施設運営</li>
+                            <li>BBQ施設コンサル業務</li>
+                            <li>BBQ施設集客斡旋業務</li>
+                            <li>BBQデリバリーサービス</li>
+                            <li>BBQ用地、BBQ施設、売買及び賃貸斡旋</li>
+                        </ul>
+                    </dd>
+
+
+                    <dt>法人設立年月日</dt>
+                    <dd>令和5年3月13日</dd>
+
+                    <dt>従業員数</dt>
+                    <dd>10名（パート・アルバイト含む）</dd>
+                </dl>
+            </section>
+
+
+
+            <h3>アクセス</h3>
+
+            <div><iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38562.75976265113!2d130.74257073018683!3d32.03878527947992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x353f0e87b5aa6eb7%3A0xbff7bb1f947369ed!2z44CSODg5LTQxNTUg5a6u5bSO55yM44GI44Gz44Gu5biC5LqA5rKi77yS77yY77yQ4oiS77yT!5e0!3m2!1sja!2sjp!4v1680744173417!5m2!1sja!2sjp"
+                    width="640" height="380" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                <p style="margin-top: 10px;">〒889-4155 宮崎県えびの市大字亀沢280-3</p>
+
+        </article>
+
+
+        <footer>
+            <a href="#wrapper" id="topbtn"><img src="resources/images/to_top.png" alt="ページTOPへ" /></a>
+
+            <figure>
+                <img src="resources/images/foot_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" />
+            </figure>
+
+            <ul>
+                <li><a href=""><img src="resources/images/sns1.png" alt="Twitter" /></a></li>
+                <li><a href=""><img src="resources/images/sns2.png" alt="You tube" /></a></li>
+                <li><a href=""><img src="resources/images/sns3.png" alt="facebook" /></a></li>
+                <li><a href=""><img src="resources/images/sns4.png" alt="LINE" /></a></li>
+            </ul>
+
+            <nav>
+                <ul>
+                    <li><a href="/about.html">Baviについて</a></li>
+                    <li><a href="/contact.html">お問い合わせ</a></li>
+                    <li><a href="/company.html">運営会社</a></li>
+                </ul>
+            </nav>
+
+            <p>&copy; Copyright Bavi all right reserved.</p>
+        </footer>
+
+
+        <nav id="bottom_nav">
+            <ul>
+                <li>
+                    <a href="/">
+                        <img src="resources/images/bottom_nav1.png" alt="BBQ場" />
+                        <span>BBQ場</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/spot.html">
+                        <img src="resources/images/bottom_nav2.png" alt="食材SPOT" />
+                        <span>食材SPOT</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/column.html">
+                        <img src="resources/images/bottom_nav3.png" alt="COLUMN" />
+                        <span>COLUMN</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/like.html">
+                        <img src="resources/images/bottom_nav4.png" alt="LIKE" />
+                        <span>LIKE</span>
+                    </a>
+                </li>
+                <li>
+                    <a id="menu_open">
+                        <img src="resources/images/bottom_nav5.png" alt="MENU" />
+                        <span>MENU</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+
+        <nav id="overlay_nav">
+            <span id="close">+</span>
+
+            <div>
+                <a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
+
+                <dl>
+                    <dt>探す</dt>
+                    <dd><a href="/">BBQ場</a></dd>
+                    <dd><a href="/spot.html">周辺店舗</a></dd>
+                    <dd><a href="/column.html">コラム</a></dd>
+
+                    <dt>MyPage</dt>
+                    <dd><a href="">お気に入りページ</a></dd>
+
+                    <dt>新着情報</dt>
+                    <dd><a href="">BBQ場新着</a></dd>
+                    <dd><a href="">周辺店舗新着</a></dd>
+
+                    <dt>その他</dt>
+                    <dd><a href="/about.html">Baviについて</a></dd>
+                    <dd><a href="/company.html">運営会社</a></dd>
+                    <dd><a href="/contact.html">お問い合わせ</a></dd>
+                    <dd><a href="/company.html">運営会社</a></dd>
+                    <dd><a href="/contact.html">お問い合わせ</a></dd>
+                </dl>
+            </div>
+        </nav>
+    </div>
 </body>
+
 </html>
