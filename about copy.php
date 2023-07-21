848cc600ed6ec7127fe9de7b45909a7becd5c69f
under-rise <suzuki@under-rise.com>
2023-07-21 14:23:35 +0900

    other

---
+++ about copy.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="">
+    <title></title>
+    <link rel="stylesheet" href="resources/css/sanitize.css">
+    <link rel="stylesheet" href="resources/css/base.css">
+    <link rel="stylesheet" href="resources/css/base_sp.css">
+    <link rel="stylesheet" href="resources/css/about.css?2">
+    <link rel="stylesheet" href="resources/css/about_sp.css?1">
+    <script type="text/javascript" src="resources/js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="resources/js/config.js"></script>
+    <script type="text/javascript" src="resources/js/smoothScroll.js"></script>
+    <!--[if lt IE 9]>
+	<script type="text/javascript" src="resources/js/html5shiv.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+    <div id="loading"></div>
+    <div id="wrapper">
+        <header class="no_fixed">
+            <h1>日本最大級のBBQプラットフォーム Bavi</h1>
+
+            <a href="/"><img src="resources/images/head_logo.png" alt="日本最大級のBBQプラットフォーム Bavi" /></a>
+        </header>
+
+
+        <article class="no_fixed cont">
+            <h2>Baviについて</h2>
+
+            <figure><img src="resources/images/about/mv.png" alt="気の合う仲間とBaviしよう" /></figure>
+
+            <section id="cont1" class="cont">
+                <div>
+                    <h3>Baviからのご挨拶</h3>
+
+                    <img src="resources/images/about/cont1_1.png" alt="イメージ" class="only_smart" />
+
+                    <p>初めまして、Bavi（BBQ×NAVI）です。<br />Bavi（バービー）はBBQ場とその周辺にある食材スポット、役立つ情報を検索できるサイトです。</p>
+
+                    <p>行きたい会場と合わせて、せっかくならその土地の美味しいものも探してみてください。あたらしい発見があるかもしれません。<br />WITH
+                        コロナの世の中だからこそ難しくなった「集う」ということ。「BBQ」という形で皆で楽しく過ごせたら良いですよね。</p>
+                </div>
+            </section>
+
+
+            <section id="cont2" class="cont">
+                <div>
+                    <h3>BAVIの由来と思い</h3>
+
+                    <img src="resources/images/about/cont2_1.png" alt="イメージ" class="only_smart" />
+
+                    <p>BBQは国によってもスタイルが異なりますが、オーストラリアでは、BBQの愛称として、「Babie（バービー）」と呼んでいます。コンロが公園に常設されているほど、身近なものとして文化が根付いています。
+                    </p>
+
+                    <p>そんな風に日本でも、もっと手軽に、気軽にBBQを楽しんでほしいという願いが<span>【BAVI（バービー）】</span>には込められているんです。</p>
+                </div>
+            </section>
+
+
+            <section id="cont3" class="cont">
+                <div>
+                    <h3>10年以上続くBBQサービスの経験</h3>
+
+                    <img src="resources/images/about/cont3_1.png" alt="イメージ" class="only_smart" />
+
+                    <p>こういう、大変な世の中だからこそ、「一人勝ち」ではなく、「みんながみんなのために」そう動けたら、もっと良い世界になりますよね。</p>
+
+                    <p>10年以上続くBBQサービスの経験を活かして、信頼できる情報をお届けします。</p>
+
+                    <p>これからもBAVIは進化し続けます！</p>
+
+                    <p>チームBAVI一同。</p>
+                </div>
+            </section>
+
+            <ul>
+                <li><a href="https://bavi.jp/assets/pdf/publication_materials_2ndseason.pdf" target="_blank">掲載詳細を確認する</a></li>
+                <li><a href="/contact.html">掲載依頼をする</a></li>
+            </ul>
+        </article>
+
+
+        <?php include __DIR__ . '/tpl/footer.php'; ?>
+
+</body>
+
+</html>
